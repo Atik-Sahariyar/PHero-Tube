696c4f2c1f3db4829ef916d70e8a83efab256791
Atik Sahariyar <atiksahariyar88@gmail.com>
2023-09-03 14:42:02 +0600

    added style

--- js/index.js
+++ js/index.js
@@ -8,15 +8,21 @@ const handleCategory = async () => {
 
     let categoryName = document.getElementById('category-name');
     const categorys = data.data;
-    categorys.forEach((category) => {
+    categorys.forEach((category, index) => {
         let div = document.createElement('div');
         div.innerHTML = `
-        <button id ="category-button" onclick = "handleLoadVideos('${category.category_id}') "  class="px-3 py-2 rounded-md bg-gray-200">${category.category}</button>
-        `;
+                <button id="category-button-${category.category_id}" onclick="handleLoadVideos('${category.category_id}')" class="px-3 py-2 rounded-md bg-gray-200">${category.category}</button>
+                `;
         categoryName.appendChild(div);
+
+        // Check if it's the first button and click it
+        if (index === 0) {
+            const firstButton = document.getElementById(`category-button-${category.category_id}`);
+            firstButton.click();
+        }
     });
- 
 };
+
 
 document.getElementById('sort-by-view').addEventListener('click', () => {
     videoData.sort((a, b) => {
@@ -33,38 +39,36 @@ const handleLoadVideos = async (categoryId) => {
         `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
     );
     const data = await response.json();
-     
+
 
     videoData = [...data.data];
     renderVideo();
 
     //if No data found
-    const emptyData = () =>{
+    const emptyData = () => {
         const cardsContainer = document.getElementById('empty-data');
         cardsContainer.innerHTML = '';
         const div = document.createElement('div');
         cardsContainer.appendChild(div);
-        if(data.data.length === 0){
-          
+        if (data.data.length === 0) {
+
             const div2 = document.createElement('div');
             div2.classList.add("flex", "text-center", "justify-center")
-             div2.innerHTML = `
+            div2.innerHTML = `
              <div class =" mt-20">
              <img src="./js/not-found.png" alt="" class = "w-16 h-16 ml-14 mb-3">
              <p>Oops!! Sorry, There is no <br> content here</p>
              </div>
              `
-             div.appendChild(div2);
-            }   
+            div.appendChild(div2);
+        }
     }
-    emptyData();
 
-   
-    
+    emptyData();
 };
 
 const renderVideo = () => {
-   
+
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = '';
     videoData?.forEach((videoData) => {
@@ -77,7 +81,7 @@ const renderVideo = () => {
                          <figure class =" ">
                            <img src=${videoData?.thumbnail} alt="" class = "w-[394px] h-[190px] rounded-lg ">
                           </figure>
-                         <p class = "text-end relative mr-3 ml-auto -mt-7 "> <span id = "upload-time" class =" text-white w-auto px-1 ${uploadTime?' bg-black': ''} rounded">${uploadTime?uploadTime: ''}</span> </p>
+                         <p class = "text-end relative mr-3 ml-auto -mt-7 "> <span id = "upload-time" class =" text-white w-auto px-1 ${uploadTime ? ' bg-black' : ''} rounded">${uploadTime ? uploadTime : ''}</span> </p>
                          </div>
                    
                                <div class="avater online flex gap-2 p-2">
@@ -88,7 +92,7 @@ const renderVideo = () => {
                                     <h4 class="font-bold ">${videoData?.title}<h4>
                                     <div class = "flex gap-1">
                                     <p>${videoData?.authors[0]?.profile_name}</p>
-                                    ${videoData?.authors[0]?.verified? `<img src="./js/verify.png" class = "w-6 h-6" alt="">`: ''}
+                                    ${videoData?.authors[0]?.verified ? `<img src="./js/verify.png" class = "w-6 h-6" alt="">` : ''}
                                     </div>
                                     <p>${videoData?.others?.views} views</p>
                                    </div>
@@ -115,3 +119,12 @@ const renderVideo = () => {
 
 
 handleCategory();
+
+// blogs
+const blogButton = document.getElementById('blog-button');
+
+blogButton.addEventListener('click', function(){
+    const blogPageURL  = './blog.html';
+
+    window.open(blogPageURL);
+})
